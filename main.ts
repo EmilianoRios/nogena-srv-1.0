e69937b2f40568a99eaee98df7bbac31a4e8a984
EmilianoRios <emiliano_luisrios@hotmail.com>
2023-04-25 00:04:03 -0300

    DenoDeploy0.27

--- main.ts
+++ main.ts
@@ -25,7 +25,7 @@ router.use(prefix + 'post', routesPost.routes())
 router.use(prefix + 'comment', routesComment.routes())
 
 app.use(router.routes())
-app.use(oakCors())
+app.use(oakCors({ origin: '*' }))
 app.use(routesEmployee.allowedMethods())
 
 await app.listen({ port: 8000 })
